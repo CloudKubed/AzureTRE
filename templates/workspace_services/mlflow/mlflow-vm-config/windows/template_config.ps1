d71684731ce5cc8b543a69c9ddc37821f5cb3041
gauravmishrack <gaurav.mishra@cloudkubed.com>
2024-10-28 13:10:48 +0530

    requiremts.txt file updated

--- templates/workspace_services/mlflow/mlflow-vm-config/windows/template_config.ps1
+++ templates/workspace_services/mlflow/mlflow-vm-config/windows/template_config.ps1
@@ -1,4 +1,4 @@
 [Environment]::SetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING", "${MLFlow_Connection_String}", "Machine")
-pip install mlflow==1.24.0
-pip install azure-storage-blob==12.10.0
-pip install azure-identity==1.8.0
+pip install mlflow==2.17.1
+pip install azure-storage-blob==12.23.1
+pip install azure-identity==1.19.0
